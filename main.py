a70d6edb301b9ee334f4ae109e0f08b0dae1243c
André <tdc@gmx.ch>
2022-11-02 20:54:29 +0100

    add azure resource

--- main.py
+++ main.py
@@ -3,42 +3,115 @@ import json
 
 def hello_server(name, network):
     return {
-        'resource': [
+    "provider": [
+        {
+        "azurerm": [
             {
-                'google_compute_instance': [
-                    {
-                        name: [
-                            {
-                                'allow_stopping_for_update': True,
-                                'zone': 'us-central1-a',
-                                'boot_disk': [
-                                    {
-                                        'initialize_params': [
-                                            {
-                                                'image': 'ubuntu-1804-lts'
-                                            }
-                                        ]
-                                    }
-                                ],
-                                'machine_type': 'e2-micro',
-                                'name': name,
-                                'network_interface': [
-                                    {
-                                        'network': network
-                                    }
-                                ],
-                                'labels': {
-                                    'name': name,
-                                    'purpose': 'manning-infrastructure-as-code'
-                                }
-                            }
-                        ]
-                    }
-                ]
+            "features": [
+                {}
+            ]
             }
         ]
+        }
+    ],
+  "resource": [
+    {
+      "azurerm_resource_group": [
+        {
+          "example": [
+            {
+              "location": "West Europe",
+              "name": "example-resources"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "azurerm_virtual_network": [
+        {
+          "example": [
+            {
+              "address_space": [
+                "10.0.0.0/16"
+              ],
+              "location": "West Europe",
+              "name": "example-network",
+              "resource_group_name": "example-resources"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "azurerm_subnet": [
+        {
+          "example": [
+            {
+              "address_prefixes": [
+                "10.0.2.0/24"
+              ],
+              "name": "internal",
+              "resource_group_name": "example-resources",
+              "virtual_network_name": "example-network"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "azurerm_network_interface": [
+        {
+          "example": [
+            {
+              "ip_configuration": [
+                {
+                  "name": "internal",
+                  "private_ip_address_allocation": "Dynamic"
+                }
+              ],
+              "location": "West Europe",
+              "name": "example-nic",
+              "resource_group_name": "example-resources"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "azurerm_linux_virtual_machine": [
+        {
+          "example": [
+            {
+              "admin_username": "adminuser",
+              "network_interface_ids": [
+                "${azurerm_network_interface.example.id}"
+              ],
+              "location": "West Europe",
+              "name": "example-machine",
+              "os_disk": [
+                {
+                  "caching": "ReadWrite",
+                  "storage_account_type": "Standard_LRS"
+                }
+              ],
+              "resource_group_name": "example-resources",
+              "size": "Standard_F2",
+              "source_image_reference": [
+                {
+                  "offer": "UbuntuServer",
+                  "publisher": "Canonical",
+                  "sku": "16.04-LTS",
+                  "version": "latest"
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
-
+  ]
+}
 
 if __name__ == "__main__":
     config = hello_server(name='hello-world', network='default')
