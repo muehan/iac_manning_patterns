cfd64cbeec3f1ec7e94ee859ff4989353ab78ff8
André <tdc@gmx.ch>
2022-11-03 20:01:04 +0100

    use a builder for vm

--- main.py
+++ main.py
@@ -1,114 +1,124 @@
 import json
 
-def hello_server(name, network, publicKey):
+class LinuxServerAzure:
+  def __init__(self, location="West Europe"):
+    self.name = 'linuxvm'
+    self.location = location
+    with open('C:\\Users\\tdc\\.ssh\\id_rsa.pub') as f:
+        self.key = f.readline()
+    self.reource = self._build()
+
+  def _build(self):
     return {
     "provider": [
-        {
-        "azurerm": [
             {
-            "features": [
-                {}
+            "azurerm": [
+                {
+                "features": [
+                    {}
+                ]
+                }
             ]
             }
-        ]
-        }
-    ],
-  "resource": [
-    {
-      "azurerm_resource_group": [
+        ],
+      "resource": [
         {
-          "example": [
+          "azurerm_resource_group": [
             {
-              "location": "West Europe",
-              "name": "example-resources"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "azurerm_virtual_network": [
-        {
-          "example": [
-            {
-              "address_space": [
-                "10.0.0.0/16"
-              ],
-              "location": "West Europe",
-              "name": "example-network",
-              "resource_group_name": "example-resources"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "azurerm_subnet": [
-        {
-          "example": [
-            {
-              "address_prefixes": [
-                "10.0.2.0/24"
-              ],
-              "name": "internal",
-              "resource_group_name": "example-resources",
-              "virtual_network_name": "${azurerm_virtual_network.example.name}"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "azurerm_network_interface": [
-        {
-          "example": [
-            {
-              "ip_configuration": [
+              "example": [
                 {
+                  "location": self.location,
+                  "name": "example-resources"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "azurerm_virtual_network": [
+            {
+              "example": [
+                {
+                  "address_space": [
+                    "10.0.0.0/16"
+                  ],
+                  "location": self.location,
+                  "name": "example-network",
+                  "resource_group_name": "example-resources"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "azurerm_subnet": [
+            {
+              "example": [
+                {
+                  "address_prefixes": [
+                    "10.0.2.0/24"
+                  ],
                   "name": "internal",
-                  "subnet_id": "${azurerm_subnet.example.id}",
-                  "private_ip_address_allocation": "Dynamic"
+                  "resource_group_name": "example-resources",
+                  "virtual_network_name": "${azurerm_virtual_network.example.name}"
                 }
-              ],
-              "location": "West Europe",
-              "name": "example-nic",
-              "resource_group_name": "example-resources"
+              ]
             }
           ]
-        }
-      ]
-    },
-    {
-      "azurerm_linux_virtual_machine": [
+        },
         {
-          "example": [
+          "azurerm_network_interface": [
             {
-              "admin_username": "adminuser",
-              "network_interface_ids": [
-                "${azurerm_network_interface.example.id}"
-              ],
-              "location": "West Europe",
-              "name": "example-machine",
-              "admin_ssh_key": [
+              "example": [
                 {
-                  "username": "adminuser",
-                  "public_key": publicKey
+                  "ip_configuration": [
+                    {
+                      "name": "internal",
+                      "subnet_id": "${azurerm_subnet.example.id}",
+                      "private_ip_address_allocation": "Dynamic"
+                    }
+                  ],
+                  "location": self.location,
+                  "name": "example-nic",
+                  "resource_group_name": "example-resources"
                 }
-              ],
-              "os_disk": [
+              ]
+            }
+          ]
+        },
+        {
+          "azurerm_linux_virtual_machine": [
+            {
+              "example": [
                 {
-                  "caching": "ReadWrite",
-                  "storage_account_type": "Standard_LRS"
-                }
-              ],
-              "resource_group_name": "example-resources",
-              "size": "Standard_F2",
-              "source_image_reference": [
-                {
-                  "offer": "UbuntuServer",
-                  "publisher": "Canonical",
-                  "sku": "16.04-LTS",
-                  "version": "latest"
+                  "admin_username": "adminuser",
+                  "network_interface_ids": [
+                    "${azurerm_network_interface.example.id}"
+                  ],
+                  "location": self.location,
+                  "name": self.name,
+                  "admin_ssh_key": [
+                    {
+                      "username": "adminuser",
+                      "public_key": self.key
+                    }
+                  ],
+                  "os_disk": [
+                    {
+                      "caching": "ReadWrite",
+                      "storage_account_type": "Standard_LRS"
+                    }
+                  ],
+                  "resource_group_name": "example-resources",
+                  "size": "Standard_F2",
+                  "source_image_reference": [
+                    {
+                      "offer": "UbuntuServer",
+                      "publisher": "Canonical",
+                      "sku": "16.04-LTS",
+                      "version": "latest"
+                    }
+                  ]
                 }
               ]
             }
@@ -116,14 +126,9 @@ def hello_server(name, network, publicKey):
         }
       ]
     }
-  ]
-}
 
 if __name__ == "__main__":
-    with open('C:\\Users\\tdc\\.ssh\\id_rsa.pub') as f:
-        key = f.readline()
-
-    config = hello_server(name='hello-world', network='default', publicKey=key)
+    vm = LinuxServerAzure()
 
     with open('main.tf.json', 'w') as outfile:
-        json.dump(config, outfile, sort_keys=True, indent=4)
+        json.dump(vm.reource, outfile, sort_keys=True, indent=4)
